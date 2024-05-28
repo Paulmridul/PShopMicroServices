cf48a331ceaa64fed4947055ed5ad413471a4dc4
Mridul Paul <paul.mridul1989@email.com>
2024-05-28 18:08:03 +0530

    Ordering services implemented

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,24 +1,38 @@
-﻿using System.Reflection;
+﻿using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Reflection;
 
 namespace Ordering.API
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
-            //services.AddCarter();
+            services.AddCarter();
+
+            services.AddExceptionHandler<CustomExceptionHandler>();
 
             services.AddMediatR(cfg =>
             {
                 cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
             });
 
+            services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("Database")!);
             return services;
         }
 
         public static WebApplication UseApiServices(this WebApplication app)
         {
-            //app.MapCarter();
+            app.MapCarter();
+            app.UseExceptionHandler(options => { });
+
+            app.UseHealthChecks("/health",
+           new HealthCheckOptions
+           {
+               ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+           });
 
             return app;
         }
