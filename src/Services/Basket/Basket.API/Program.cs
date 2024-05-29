2f9f2f6a39aa4c57b9157a72d0bd4b17271d49d8
Mridul Paul <paul.mridul1989@email.com>
2024-05-29 23:02:09 +0530

    Ordering microservice completed

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -3,6 +3,7 @@ using BuildingBlocks.Exceptions.Handler;
 using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using BuildingBlocks.Messaging.MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -52,7 +53,8 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
     return handler;
 });
 
-
+//Async Communication Services
+builder.Services.AddMessageBroker(builder.Configuration);
 
 var app = builder.Build();
 
